aa7cebdc5706c4adeb86ee60f8f21b0647cd9fd7
Michael <mkenton2020@icloud.com>
2021-09-17 14:50:48 -0500

    more CSS style on Lab component

--- src/components/Lab.js
+++ src/components/Lab.js
@@ -6,8 +6,8 @@ function Lab({ lab, handleUpdate, handleDelete }) {
         <div className="lab-info">
             <p className="lab-name">{lab.name}</p>
             <p><strong>{lab.student.user_name}</strong></p>
-            <p>Number of Commits: {lab.num_commits}</p>
-            <p>Submitted at: {lab.time_of_commit}</p>
+            <p>Number of Commits: <em>{lab.num_commits}</em></p>
+            <p>Submitted at: <em>{lab.time_of_commit}</em></p>
             <p style={{ 'color': lab.completed==="Yes" ? 'green' : 'red'}}> {lab.completed==="Yes" ? "Completed" : "Incomplete"}</p>
             <button style={{ 'color': lab.tasked ? 'white' : '', 'backgroundColor': lab.tasked ? 'green' : '' }} onClick={() => handleUpdate(lab)} className="buttons">{lab.tasked ? "Tasked!" : "Add to Tasks"}</button>
             <button onClick={() => handleDelete(lab.id)} className="buttons">Remove lab</button>
